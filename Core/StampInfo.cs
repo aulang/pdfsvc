fccd923e775cc268b45e15db1c567af1dbb0e5d5
Aulang <aulang@qq.com>
2021-02-10 20:36:00 +0800

    BUG

--- Core/StampInfo.cs
+++ Core/StampInfo.cs
@@ -12,43 +12,38 @@ namespace pdfsvc.Core
 
         public StampInfo(string regex, List<int> pages, bool latest = true)
         {
-            if (regex == null || regex == "")
-            {
-                Regex = "盖章";
-            }
-            else
+            if (regex != null && regex == "")
             {
                 Regex = regex;
             }
 
             if (pages == null || pages.Count == 0)
             {
-                Pages = new List<int>();
                 pages.Add(-1);
             }
             else
             {
-                Pages = pages;
+                Pages.AddRange(pages);
             }
 
             Latest = latest;
         }
 
         // 盖章日期，可为null
-        public long SignDate { get; set; }
+        public long SignDate { get; set; } = 0;
         // 盖章理由，可为null
-        public string Reason { get; set; }
+        public string Reason { get; set; } = "";
         // 盖章位置，可为null
-        public string Location { get; set; }
+        public string Location { get; set; } = "";
         // 盖章人，可为null
-        public string Creator { get; set; }
+        public string Creator { get; set; } = "";
         // 盖章人联系方式，可为null
-        public string Contact { get; set; }
+        public string Contact { get; set; } = "";
         // 盖章人联系方式，可为null
-        public List<Int32> Pages { get; set; }
+        public List<int> Pages { get; set; } = new List<int>();
         // 盖章关键字查找正则表达式
-        public string Regex { get; set; }
+        public string Regex { get; set; } = "盖章";
         // 是否最后一个关键字处盖章，TODO 暂不支持一次盖多个章
-        public bool Latest { get; set; }
+        public bool Latest { get; set; } = true;
     }
 }
