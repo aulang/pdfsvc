7d7c50c5ada088b59841d999a5a1c36f30f4541b
Aulang <aulang@qq.com>
2021-02-28 18:00:58 +0800

    去除PDF签名盖章

--- Controllers/PdfController.cs
+++ Controllers/PdfController.cs
@@ -5,11 +5,8 @@ using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System;
-using System.Web;
 using System.IO;
-using System.Text;
 using pdfsvc.Business;
-using pdfsvc.Core;
 
 namespace pdfsvc.Controllers
 {
@@ -22,30 +19,20 @@ namespace pdfsvc.Controllers
     {
         private readonly ILogger<PdfController> _logger;
         private readonly FileManager _fileManager;
-        private readonly FdfManager _pdfManager;
 
-        public PdfController(ILogger<PdfController> logger, FileManager fileManager, FdfManager pdfManager)
+        public PdfController(ILogger<PdfController> logger, FileManager fileManager)
         {
             _logger = logger;
             _fileManager = fileManager;
-            _pdfManager = pdfManager;
         }
 
         /// <summary>
         /// Office格式(word、excel、ppt)文件转PDF
         /// </summary>
         /// <param name="file">PDF文件</param>
-        /// <param name="sign">是否签名</param>
-        /// <param name="regex">签名标记正则表达式，默认“盖章”</param>
-        /// <param name="pages">标记查找页码，负数倒数页码, 默认-1</param>
-        /// <param name="latest">是否在最后一个标记处签名</param>
         /// <returns>PDF文件</returns>
         [HttpPost("convert")]
-        public async Task<IActionResult> Convert([Required] IFormFile file,
-            [FromForm] bool sign = false,
-            [FromForm] string regex = null,
-            [FromForm] List<int> pages = null,
-            [FromForm] bool latest = true)
+        public async Task<IActionResult> Convert([Required] IFormFile file)
         {
             // 文件名
             string fileName = Path.GetFileName(file.FileName);
@@ -71,62 +58,8 @@ namespace pdfsvc.Controllers
                 return Problem(e.StackTrace, e.Message, StatusCodes.Status500InternalServerError);
             }
 
-            if (sign)
-            {
-                // PDF签名盖章
-                using FileStream pdf = new FileStream(outputFilePath, FileMode.Open);
-                outputFilePath = outputFilePath.Replace(".pdf", "_signed.pdf");
-                using FileStream outPdf = new FileStream(outputFilePath, FileMode.Create);
-
-                StampInfo stampInfo = new StampInfo(regex, pages, latest);
-
-                _pdfManager.Sign(pdf, outPdf, stampInfo);
-            }
-
             // 更改文件后缀
             fileName = Path.ChangeExtension(fileName, FileManager.PDF);
-            // 文件名UTF-8编码
-            // fileName = HttpUtility.UrlEncode(fileName, Encoding.UTF8);
-
-            return PhysicalFile(outputFilePath, "application/pdf", fileName);
-        }
-
-        /// <summary>
-        /// PDF文件签名盖章
-        /// </summary>
-        /// <param name="file">PDF文件</param>
-        /// <param name="regex">签名标记正则表达式，默认“盖章”</param>
-        /// <param name="pages">标记查找页码，负数倒数页码, 默认-1</param>
-        /// <param name="latest">是否在最后一个标记处签名</param>
-        /// <returns>PDF文件</returns>
-        [HttpPost("sign")]
-        public async Task<IActionResult> Sign([Required] IFormFile file,
-            [FromForm] string regex = null,
-            [FromForm] List<int> pages = null,
-            [FromForm] bool latest = true)
-        {
-            _logger.LogInformation("PDF文件签名盖章");
-
-            // 文件名
-            string fileName = Path.GetFileName(file.FileName);
-            // 上传Office文件保持路径
-            string inputFilePath = _fileManager.GetInputPdfPath(fileName);
-
-            using (FileStream stream = new FileStream(inputFilePath, FileMode.Create))
-            {
-                await file.CopyToAsync(stream);
-            }
-
-            string outputFilePath = inputFilePath.Replace(".pdf", "_signed.pdf");
-
-            // PDF签名盖章
-            using FileStream pdf = new FileStream(inputFilePath, FileMode.Open);
-            using FileStream outPdf = new FileStream(outputFilePath, FileMode.Create);
-
-            StampInfo stampInfo = new StampInfo(regex, pages, latest);
-
-            _pdfManager.Sign(pdf, outPdf, stampInfo);
-
             // 文件名UTF-8编码
             // fileName = HttpUtility.UrlEncode(fileName, Encoding.UTF8);
 
--- README.md
+++ README.md
@@ -1,23 +1,10 @@
-# PDFService
-Office格式文件（支持doc、docx、xls、xlsx、ppt、pptx）转换为PDF格式文件、PDF文件签名盖章服务。  
-PDF转换使用Office COM组件实现，兼容性好，PDF文件签名盖章基于iText7实现。
+# PdfSvc
+Office格式文件（支持doc、docx、xls、xlsx、ppt、pptx）转换为PDF格式文件
 
 ### 1. PDF转换
 1. 请求地址：/pdf/convert
 2. 请求参数  
-    file: Office格式文件  
-    sign:布尔值，true：签名盖章；false：不签名盖章。默认false  
-    regex: 签名标记正则表达式，默认“盖章”
-    pages: 标记查找页码，负数倒数页码, 默认-1
-    latest: 是否在最后一个标记处签名
-
-    
-### 2. PDF签名
-1. 请求地址：/pdf/sign
-2. 请求参数  
-    file: PDF格式文件  
-    regex: 签名标记正则表达式，默认“盖章”
-    pages: 标记查找页码，负数倒数页码, 默认-1
-    latest: 是否在最后一个标记处签名
+    file: Office格式文件
+3. 返回值PDF文件
 
 `接口详见swagger(http://localhost:5000/index.html)`
